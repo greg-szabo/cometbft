7be079ec37eb4aa81f5df0dfe2cb1f0b947d22b4
Alessandro Sforzin <alessandro@informal.systems>
2025-03-14 13:57:27 +0100

    Add `BlobID` to `Proposal` protobuf.
    Regenerated the proto files as well.
    Moved the proto definition of `BlobID` to the `types.proto` file to fix an
    import cycle.

--- proto/tendermint/types/blob.pb.go
+++ proto/tendermint/types/blob.pb.go
@@ -5,7 +5,6 @@ package types
 
 import (
 	fmt "fmt"
-	_ "github.com/cosmos/gogoproto/gogoproto"
 	proto "github.com/cosmos/gogoproto/proto"
 	io "io"
 	math "math"
@@ -84,83 +83,24 @@ func (m *Blob) GetData() []byte {
 	return nil
 }
 
-// BlobID defines the unique ID of a blob as its hash and its PartSetHeader.
-type BlobID struct {
-	Hash          []byte        `protobuf:"bytes,1,opt,name=hash,proto3" json:"hash,omitempty"`
-	PartSetHeader PartSetHeader `protobuf:"bytes,2,opt,name=part_set_header,json=partSetHeader,proto3" json:"part_set_header"`
-}
-
-func (m *BlobID) Reset()         { *m = BlobID{} }
-func (m *BlobID) String() string { return proto.CompactTextString(m) }
-func (*BlobID) ProtoMessage()    {}
-func (*BlobID) Descriptor() ([]byte, []int) {
-	return fileDescriptor_06b000d333db09d8, []int{1}
-}
-func (m *BlobID) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *BlobID) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_BlobID.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *BlobID) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_BlobID.Merge(m, src)
-}
-func (m *BlobID) XXX_Size() int {
-	return m.Size()
-}
-func (m *BlobID) XXX_DiscardUnknown() {
-	xxx_messageInfo_BlobID.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_BlobID proto.InternalMessageInfo
-
-func (m *BlobID) GetHash() []byte {
-	if m != nil {
-		return m.Hash
-	}
-	return nil
-}
-
-func (m *BlobID) GetPartSetHeader() PartSetHeader {
-	if m != nil {
-		return m.PartSetHeader
-	}
-	return PartSetHeader{}
-}
-
 func init() {
 	proto.RegisterType((*Blob)(nil), "tendermint.types.Blob")
-	proto.RegisterType((*BlobID)(nil), "tendermint.types.BlobID")
 }
 
 func init() { proto.RegisterFile("tendermint/types/blob.proto", fileDescriptor_06b000d333db09d8) }
 
 var fileDescriptor_06b000d333db09d8 = []byte{
-	// 233 bytes of a gzipped FileDescriptorProto
+	// 145 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2e, 0x49, 0xcd, 0x4b,
 	0x49, 0x2d, 0xca, 0xcd, 0xcc, 0x2b, 0xd1, 0x2f, 0xa9, 0x2c, 0x48, 0x2d, 0xd6, 0x4f, 0xca, 0xc9,
-	0x4f, 0xd2, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x40, 0x48, 0xea, 0x81, 0x25, 0xa5, 0x64,
-	0x30, 0x94, 0x83, 0x49, 0x88, 0x7a, 0x29, 0x91, 0xf4, 0xfc, 0xf4, 0x7c, 0x30, 0x53, 0x1f, 0xc4,
-	0x82, 0x88, 0x2a, 0x49, 0x71, 0xb1, 0x38, 0xe5, 0xe4, 0x27, 0x09, 0x09, 0x71, 0xb1, 0xa4, 0x24,
-	0x96, 0x24, 0x4a, 0x30, 0x2a, 0x30, 0x6a, 0xf0, 0x04, 0x81, 0xd9, 0x4a, 0xd9, 0x5c, 0x6c, 0x20,
-	0x39, 0x4f, 0x17, 0x90, 0x6c, 0x46, 0x62, 0x71, 0x06, 0x4c, 0x16, 0xc4, 0x16, 0xf2, 0xe5, 0xe2,
-	0x2f, 0x48, 0x2c, 0x2a, 0x89, 0x2f, 0x4e, 0x2d, 0x89, 0xcf, 0x48, 0x4d, 0x4c, 0x49, 0x2d, 0x92,
-	0x60, 0x52, 0x60, 0xd4, 0xe0, 0x36, 0x92, 0xd7, 0x43, 0x77, 0x99, 0x5e, 0x40, 0x62, 0x51, 0x49,
-	0x70, 0x6a, 0x89, 0x07, 0x58, 0x99, 0x13, 0xcb, 0x89, 0x7b, 0xf2, 0x0c, 0x41, 0xbc, 0x05, 0x28,
-	0x82, 0xbe, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c, 0xe3, 0x84,
-	0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31, 0xdc, 0x78, 0x2c, 0xc7, 0x10, 0x65, 0x9c, 0x9e, 0x59,
-	0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f, 0xab, 0x9f, 0x9c, 0x9f, 0x9b, 0x5a, 0x92, 0x94, 0x56,
-	0x82, 0x60, 0x40, 0x7c, 0x84, 0xee, 0xf3, 0x24, 0x36, 0xb0, 0xb8, 0x31, 0x20, 0x00, 0x00, 0xff,
-	0xff, 0x98, 0x0c, 0xf6, 0x93, 0x43, 0x01, 0x00, 0x00,
+	0x4f, 0xd2, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x40, 0x48, 0xea, 0x81, 0x25, 0x95, 0xa4,
+	0xb8, 0x58, 0x9c, 0x72, 0xf2, 0x93, 0x84, 0x84, 0xb8, 0x58, 0x52, 0x12, 0x4b, 0x12, 0x25, 0x18,
+	0x15, 0x18, 0x35, 0x78, 0x82, 0xc0, 0x6c, 0x27, 0xdf, 0x13, 0x8f, 0xe4, 0x18, 0x2f, 0x3c, 0x92,
+	0x63, 0x7c, 0xf0, 0x48, 0x8e, 0x71, 0xc2, 0x63, 0x39, 0x86, 0x0b, 0x8f, 0xe5, 0x18, 0x6e, 0x3c,
+	0x96, 0x63, 0x88, 0x32, 0x4e, 0xcf, 0x2c, 0xc9, 0x28, 0x4d, 0xd2, 0x4b, 0xce, 0xcf, 0xd5, 0x4f,
+	0xce, 0xcf, 0x4d, 0x2d, 0x49, 0x4a, 0x2b, 0x41, 0x30, 0xc0, 0x76, 0xe9, 0xa3, 0xbb, 0x23, 0x89,
+	0x0d, 0x2c, 0x6e, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0xfc, 0x99, 0x72, 0x66, 0xa2, 0x00, 0x00,
+	0x00,
 }
 
 func (m *Blob) Marshal() (dAtA []byte, err error) {
@@ -193,46 +133,6 @@ func (m *Blob) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
-func (m *BlobID) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *BlobID) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *BlobID) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	{
-		size, err := m.PartSetHeader.MarshalToSizedBuffer(dAtA[:i])
-		if err != nil {
-			return 0, err
-		}
-		i -= size
-		i = encodeVarintBlob(dAtA, i, uint64(size))
-	}
-	i--
-	dAtA[i] = 0x12
-	if len(m.Hash) > 0 {
-		i -= len(m.Hash)
-		copy(dAtA[i:], m.Hash)
-		i = encodeVarintBlob(dAtA, i, uint64(len(m.Hash)))
-		i--
-		dAtA[i] = 0xa
-	}
-	return len(dAtA) - i, nil
-}
-
 func encodeVarintBlob(dAtA []byte, offset int, v uint64) int {
 	offset -= sovBlob(v)
 	base := offset
@@ -254,21 +154,6 @@ func (m *Blob) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovBlob(uint64(l))
 	}
-	return n
-}
-
-func (m *BlobID) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	l = len(m.Hash)
-	if l > 0 {
-		n += 1 + l + sovBlob(uint64(l))
-	}
-	l = m.PartSetHeader.Size()
-	n += 1 + l + sovBlob(uint64(l))
 	return n
 }
 
@@ -339,123 +224,6 @@ func (m *Blob) Unmarshal(dAtA []byte) error {
 			m.Data = append(m.Data[:0], dAtA[iNdEx:postIndex]...)
 			if m.Data == nil {
 				m.Data = []byte{}
-			}
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipBlob(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthBlob
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *BlobID) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowBlob
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: BlobID: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: BlobID: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Hash", wireType)
-			}
-			var byteLen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowBlob
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				byteLen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if byteLen < 0 {
-				return ErrInvalidLengthBlob
-			}
-			postIndex := iNdEx + byteLen
-			if postIndex < 0 {
-				return ErrInvalidLengthBlob
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Hash = append(m.Hash[:0], dAtA[iNdEx:postIndex]...)
-			if m.Hash == nil {
-				m.Hash = []byte{}
-			}
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field PartSetHeader", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowBlob
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthBlob
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthBlob
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if err := m.PartSetHeader.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
 			}
 			iNdEx = postIndex
 		default:
