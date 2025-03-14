656409eeefbfb501f975c1fea55e93f87002afd0
Alessandro Sforzin <alessandro@informal.systems>
2025-03-14 08:34:10 +0100

    Add `BlobID` type

--- types/blob.go
+++ types/blob.go
@@ -2,8 +2,12 @@ package types
 
 import (
 	"encoding/hex"
+	"errors"
+	"fmt"
 
+	cmtproto "github.com/cometbft/cometbft/api/cometbft/types/v1"
 	"github.com/cometbft/cometbft/crypto/tmhash"
+	cmtbytes "github.com/cometbft/cometbft/libs/bytes"
 )
 
 // Blob represents an Ethereum blob, a binary data structure introduced in
@@ -35,4 +39,88 @@ func (b Blob) String() string {
 		return "nil-Blob"
 	}
 	return "Blob#" + hex.EncodeToString(b)
+}
+
+// BlobID defines the unique ID of a block as its hash and its PartSetHeader.
+// This structure is identical to BlobID, but we create a separate type to avoid
+// confusion. Since BlobID and BlockID represent distinct concepts, defining a
+// dedicated type with its own methods allows for independent changes later on.
+type BlobID struct {
+	Hash          cmtbytes.HexBytes `json:"hash"`
+	PartSetHeader PartSetHeader     `json:"parts"`
+}
+
+// ValidateBasic performs basic validation.
+func (b BlobID) ValidateBasic() error {
+	if err := ValidateHash(b.Hash); err != nil {
+		return errors.New("wrong Hash")
+	}
+	if err := b.PartSetHeader.ValidateBasic(); err != nil {
+		return fmt.Errorf("wrong PartSetHeader: %w", err)
+	}
+	return nil
+}
+
+// IsNil returns true if this is the BlobID of a nil blob.
+func (b BlobID) IsNil() bool {
+	return len(b.Hash) == 0 && b.PartSetHeader.IsZero()
+}
+
+// IsComplete returns true if this is a valid BlobID of a non-nil blob.
+func (b BlobID) IsComplete() bool {
+	return len(b.Hash) == tmhash.Size &&
+		b.PartSetHeader.Total > 0 &&
+		len(b.PartSetHeader.Hash) == tmhash.Size
+}
+
+// String returns a human readable string representation of the BlobID.
+//
+// 1. hash
+// 2. part set header
+//
+// See PartSetHeader#String.
+func (b BlobID) String() string {
+	return fmt.Sprintf(`%v:%v`, b.Hash, b.PartSetHeader)
+}
+
+// ToProto converts BlobID to protobuf.
+func (b BlobID) ToProto() cmtproto.BlobID {
+	if b.IsNil() {
+		return cmtproto.BlobID{}
+	}
+
+	return cmtproto.BlobID{
+		Hash:          b.Hash,
+		PartSetHeader: b.PartSetHeader.ToProto(),
+	}
+}
+
+// BlobIDFromProto sets a protobuf BlobID to the given pointer.
+// It returns an error if the block id is invalid.
+func BlobIDFromProto(b *cmtproto.BlobID) (BlobID, error) {
+	if b == nil {
+		return BlobID{}, errors.New("nil BlobID")
+	}
+
+	ph, err := PartSetHeaderFromProto(&b.PartSetHeader)
+	if err != nil {
+		return BlobID{}, err
+	}
+
+	blobID := BlobID{
+		Hash:          b.Hash,
+		PartSetHeader: *ph,
+	}
+
+	if err := blobID.ValidateBasic(); err != nil {
+		return BlobID{}, fmt.Errorf("validating BlobID: %s", err)
+	}
+
+	return blobID, nil
+}
+
+// ProtoBlobIDIsNil is similar to the IsNil function on BlobID, but for the
+// Protobuf representation.
+func ProtoBlobIDIsNil(b *cmtproto.BlobID) bool {
+	return len(b.Hash) == 0 && ProtoPartSetHeaderIsZero(&b.PartSetHeader)
 }
