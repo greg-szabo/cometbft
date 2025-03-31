efabf3b3bbec9f851d947e740ca7fed238841b53
Greg Szabo <greg@philosobear.com>
2025-03-31 12:57:48 -0400

    condition simplified, updateHeight clears blob

--- consensus/state.go
+++ consensus/state.go
@@ -775,7 +775,7 @@ func (cs *State) updateToState(state sm.State) {
 	cs.Proposal = nil
 	cs.ProposalBlock = nil
 	cs.ProposalBlockParts = nil
-	cs.ProposalBlob = nil
+	cs.ProposalBlob = types.Blob{}
 	cs.ProposalBlobParts = nil
 	cs.LockedRound = -1
 	cs.LockedBlock = nil
@@ -949,7 +949,7 @@ func (cs *State) handleMsg(mi msgInfo) {
 		cs.mtx.Unlock()
 
 		cs.mtx.Lock()
-		if added && cs.ProposalBlockParts.IsComplete() && ((cs.ProposalBlobParts != nil && cs.ProposalBlobParts.IsComplete()) || cs.ProposalBlobParts == nil) {
+		if added && cs.ProposalBlockParts.IsComplete() && (cs.ProposalBlobParts == nil || cs.ProposalBlobParts.IsComplete()) {
 			cs.handleCompleteProposal(msg.Height)
 		}
 		if added {
@@ -2171,7 +2171,7 @@ func (cs *State) addProposalBlockPart(msg *BlockPartMessage, peerID p2p.ID) (add
 		cs.Logger.Info("received complete proposal block", "height", cs.ProposalBlock.Height, "hash", cs.ProposalBlock.Hash())
 
 		// Both blocks and blobs need to be complete to fire the event.
-		if cs.ProposalBlobParts == nil || (cs.ProposalBlobParts != nil && cs.ProposalBlobParts.IsComplete()) {
+		if cs.ProposalBlobParts == nil || cs.ProposalBlobParts.IsComplete() {
 			if err := cs.eventBus.PublishEventCompleteProposal(cs.CompleteProposalEvent()); err != nil {
 				cs.Logger.Error("Failed publishing event complete proposal", "err", err)
 			}
