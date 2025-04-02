73dd42316c684b1982e811f40fa4dc41ea9a0110
Alessandro Sforzin <alessandro@informal.systems>
2025-04-02 10:59:17 +0200

    Set `ProposalBlob` to empty slice rather than `nil` in `enterNewRound()`

--- consensus/state.go
+++ consensus/state.go
@@ -1165,7 +1165,7 @@ func (cs *State) enterNewRound(height int64, round int32) {
 		cs.Proposal = nil
 		cs.ProposalBlock = nil
 		cs.ProposalBlockParts = nil
-		cs.ProposalBlob = nil
+		cs.ProposalBlob = types.Blob{}
 		cs.ProposalBlobParts = nil
 	}
 
